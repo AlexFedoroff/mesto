5f7ca3357946210f7f810aa1ae9f5035b7cbc9bd
Alex Fedoroff <alexanderfedorov@192-168-1-3.local>
2022-08-02 20:14:29 +0300

    scratch

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,34 @@
 var editForm = document.querySelector(".edit-form");                //Окно редактирования
+var editFormCont = document.querySelector(".edit-form__container"); //Форма редактирования
 var closeIcon = document.querySelector(".edit-form__close-icon");   //Иконка закрытия окна редактирования
 var editBtn = document.querySelector(".profile__edit-button");      //Кнопка редактирования профиля
+var titleEl = document.querySelector(".profile__info-title");       //
+var captionEl =  document.querySelector(".profile__info-caption");  //
 
 //Открытие окна редактирования
 function openEditForm()
 {    
-    editForm.style.display = "flex";
+    editForm.style.display = "flex";    
+    editFormCont.elements["fldTitle"].value = titleEl.textContent;
+    editFormCont.elements["fldCaption"].value = captionEl.textContent;
 }
 
 //Закрытие окна редактирования
 function closeEditForm()
 {        
-    editForm.style.display = "none";
+  editForm.style.display = "none";
 }
 
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    var fldTitleValue = editFormCont.elements["fldTitle"].value;
+    var fldCaptionValue = editFormCont.elements["fldCaption"].value;    
+    titleEl.textContent = fldTitleValue;
+    captionEl.textContent = fldCaptionValue;
+    closeEditForm();
+}
+
+//Обработчики событий
 editBtn.addEventListener('click',openEditForm);
 closeIcon.addEventListener('click',closeEditForm);
+editForm.addEventListener('submit', formSubmitHandler); 
