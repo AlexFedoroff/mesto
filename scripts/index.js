dcc8b79d0b805aeb4e5a9ce8f5503bad86904d3c
Alex Fedoroff <alexanderfedorov@192-168-1-3.local>
2022-08-09 19:11:21 +0300

    for_review#3

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,34 @@
-var popup       = document.querySelector(".popup");                       //Окно редактирования
-var closeIcon   = document.querySelector(".popup__close-icon");           //Иконка закрытия окна редактирования
-var editBtn     = document.querySelector(".profile__edit-button");        //Кнопка редактирования профиля
-var nameEl      = document.querySelector(".profile__info-name");         //Элемент заголовок профиля
-var descrEl     = document.querySelector(".profile__info-description");       //Элемент текст профиля
-var nameFld     = document.querySelector(".popup-form__field_kind_name"); //Поле заголовок профиля
-var descrFld    = document.querySelector(".popup-form__field_kind_description");//Поле текст профиля  
-var contBl       = document.querySelector(".content");
-var headerBl     = document.querySelector(".header");
-var footerBl     = document.querySelector(".footer");
+var popup = document.querySelector(".popup"); //Окно редактирования
+var closeIcon = document.querySelector(".popup__close-icon"); //Иконка закрытия окна редактирования
+var editBtn = document.querySelector(".profile__edit-button"); //Кнопка редактирования профиля
+var nameEl = document.querySelector(".profile__info-name"); //Элемент заголовок профиля
+var descrEl = document.querySelector(".profile__info-description"); //Элемент текст профиля
+var nameFld = document.querySelector(".popup-form__field_kind_name"); //Поле заголовок профиля
+var descrFld = document.querySelector(".popup-form__field_kind_description"); //Поле текст профиля
+var overlay = document.querySelector(".overlay"); 
 
 //Открытие окна редактирования
 function openEditForm()
 {    
-    nameFld.value = nameEl.textContent;
-    descrFld.value = descrEl.textContent; 
-
-    popup.classList.add("popup_opened");
-    headerBl.classList.add("header_muted");
-    contBl.classList.add("content_muted");
-    footerBl.classList.add("footer_muted");    
+  nameFld.value = nameEl.textContent;
+  descrFld.value = descrEl.textContent;       
+  popup.classList.add("popup_opened");
+  overlay.classList.add("overlay_opened");
 }
 
 //Закрытие окна редактирования
 function closeEditForm()
 {        
-    popup.classList.remove("popup_opened");  
-    headerBl.classList.remove("header_muted");
-    contBl.classList.remove("content_muted");
-    footerBl.classList.remove("footer_muted");
+  popup.classList.remove("popup_opened");        
+  overlay.classList.remove("overlay_opened");
 }
 
 //Изменение элементов
 function formSubmitHandler (evt) {
-    evt.preventDefault();    
-    nameEl.textContent = nameFld.value;
-    descrEl.textContent = descrFld.value;
-    closeEditForm();
+  evt.preventDefault();    
+  nameEl.textContent = nameFld.value;
+  descrEl.textContent = descrFld.value;
+  closeEditForm();
 }
 
 //Обработчики событий
